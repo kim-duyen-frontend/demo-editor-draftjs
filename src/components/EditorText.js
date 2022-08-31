d2bc27614cd80d71c461c17950b39122dcc030a2
kim-duyen-frontend <ntduyen255@gmail.com>
2022-08-31 12:59:23 +0700

    add formatting for text use richutils

--- src/components/EditorText.js
+++ src/components/EditorText.js
@@ -1,4 +1,4 @@
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import React, { Component } from 'react';
 
 class EditorText extends Component {
@@ -11,13 +11,22 @@ class EditorText extends Component {
             this.setState({ editorState })
         }
     }
+    handleFormatText(command) {
+        const { editorState } = this.state;
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return true;
+        }
+        return false;
+    }
     render() {
         const { editorState } = this.state;
         return (
             <div className="container">
                 <div className="editorText">
                     <div className="editor">
-                        <Editor editorState={editorState} onChange={this.onChange} />
+                        <Editor editorState={editorState} onChange={this.onChange} handleKeyCommand={this.handleFormatText.bind(this)} />
                     </div>
                 </div>
             </div>
--- src/components/EditorTextHook.js
+++ src/components/EditorTextHook.js
@@ -1,15 +1,27 @@
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import React from 'react';
 import { useState } from 'react';
 
 const EditorText = () => {
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
+    const handleFormatText = (command) => {
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState) {
+            setEditorState(newState);
+            return true;
+        }
+        return false;
+    }
     return (
         <div className="container">
             <div className="editorText">
                 <div className="editor">
-                    <Editor editorState={editorState} onChange={(editorState) => setEditorState(editorState)} />
+                    <Editor
+                        editorState={editorState}
+                        onChange={(editorState) => setEditorState(editorState)}
+                        handleKeyCommand={handleFormatText}
+                    />
                 </div>
             </div>
 
