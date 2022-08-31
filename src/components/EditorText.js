fbafe14e74dac8ab4264c01afee4e127656bab69
kim-duyen-frontend <ntduyen255@gmail.com>
2022-08-31 15:10:45 +0700

    add some formatting to the text

--- src/components/EditorText.js
+++ src/components/EditorText.js
@@ -1,5 +1,6 @@
-import { Editor, EditorState, RichUtils } from 'draft-js';
 import React, { Component } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import { FaBold, FaItalic, FaUnderline, FaCode } from "react-icons/fa";
 
 class EditorText extends Component {
     constructor(props) {
@@ -20,13 +21,26 @@ class EditorText extends Component {
         }
         return false;
     }
+
     render() {
         const { editorState } = this.state;
+
         return (
             <div className="container">
                 <div className="editorText">
+                    <div>
+                        <button onClick={() => this.handleFormatText("bold")}><FaBold /></button>
+                        <button onClick={() => this.handleFormatText("italic")}><FaItalic /></button>
+                        <button onClick={() => this.handleFormatText("underline")}><FaUnderline /></button>
+                        <button onClick={() => this.handleFormatText("code")}><FaCode /></button>
+                    </div>
                     <div className="editor">
-                        <Editor editorState={editorState} onChange={this.onChange} handleKeyCommand={this.handleFormatText.bind(this)} />
+                        <Editor
+                            editorState={editorState}
+                            onChange={this.onChange}
+                            handleKeyCommand={this.handleFormatText.bind(this)}
+                            placeholder="Input your text..."
+                        />
                     </div>
                 </div>
             </div>
--- src/components/EditorTextHook.js
+++ src/components/EditorTextHook.js
@@ -1,6 +1,6 @@
-import { Editor, EditorState, RichUtils } from 'draft-js';
-import React from 'react';
 import { useState } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import { FaBold, FaItalic, FaUnderline, FaCode } from "react-icons/fa";
 
 const EditorText = () => {
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -16,11 +16,18 @@ const EditorText = () => {
     return (
         <div className="container">
             <div className="editorText">
+                <div>
+                    <button onClick={() => handleFormatText("bold")}><FaBold /></button>
+                    <button onClick={() => handleFormatText("italic")}><FaItalic /></button>
+                    <button onClick={() => handleFormatText("underline")}><FaUnderline /></button>
+                    <button onClick={() => handleFormatText("code")}><FaCode /></button>
+                </div>
                 <div className="editor">
                     <Editor
                         editorState={editorState}
                         onChange={(editorState) => setEditorState(editorState)}
                         handleKeyCommand={handleFormatText}
+                        placeholder="Input your text..."
                     />
                 </div>
             </div>
